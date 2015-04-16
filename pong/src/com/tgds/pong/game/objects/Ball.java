fbc3b1d153a136bc69a90f67e832884166224633
Rebekah Edmondson <bexedmondson@gmail.com>
2015-04-16 20:54:08 +0100

    Fixed!

--- pong/src/com/tgds/pong/game/objects/Ball.java
+++ pong/src/com/tgds/pong/game/objects/Ball.java
@@ -3,6 +3,7 @@ package com.tgds.pong.game.objects;
 import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.Shape;
+import java.awt.geom.Rectangle2D;
 
 import com.tgds.common.game.entities.GameFieldEntity;
 import com.tgds.common.game.entities.MobileGameFieldEntity;
@@ -39,10 +40,37 @@ public class Ball extends MobileGameFieldEntity {
 			double x = getVelocity().getX();
 			double y = getVelocity().getY();
 			if (other instanceof Paddle) {
-				x = - getVelocity().getX();
+				
+				Rectangle2D thisShape = this.getShapeInLocation().getBounds2D();
+				double xLocation = thisShape.getX();
+				double yLocation = thisShape.getY();
+				
+				Rectangle2D otherShape = other.getShapeInLocation().getBounds2D();
+
+				// Get the intersection rectangle to find out which way to bounce.
+		        Rectangle2D iRect = thisShape.createIntersection(otherShape);
+		        
+		        if ((xLocation+(WIDTH/2)) < (iRect.getX()+(iRect.getWidth()/2))) {
+		        	// If we hit on the left side, go left (negative x velocity).
+		        	x = -Math.abs(x);
+		        	
+		        } else if ((xLocation+(WIDTH/2)) > (iRect.getX()+(iRect.getWidth()/2))) {
+		        	// If we hit on the right side, go right (positive x velocity).
+		        	x = Math.abs(x);
+		        	
+		        } else if ((yLocation+(HEIGHT/2)) < (iRect.getY()+(iRect.getHeight()/2))) {
+		        	// If we hit on the top, go up.
+		        	y = -Math.abs(y);
+		        	
+		        } else if ((yLocation+(HEIGHT/2)) > (iRect.getY()+(iRect.getHeight()/2))) {
+		        	// If we hit on the bottom, go down.
+		        	y = Math.abs(y);
+		        }
+				
 			} else if (other instanceof Wall) {
 				y = - getVelocity().getY();
-			}
+			}	        
+			
 			setVelocity(Vector.cartesian(x, y));
 		}
 		return false;
