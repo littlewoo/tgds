854f876340faa69604c63403d87872ec0e0c6872
Rebekah Edmondson <bexedmondson@gmail.com>
2015-04-17 18:44:25 +0100

    and so it begins
    made a test class!

---
+++ pong/src/com/tgds/pong/game/controllers/PongControllerTests.java
@@ -0,0 +1,11 @@
+package com.tgds.pong.game.controllers;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class PongControllerTests {
+
+}
